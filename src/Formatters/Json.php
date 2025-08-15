c5f2cba0dda6da85fca1b09a6de2a919060d1512
Andrei Goncharenko <andrei.h@seranking.com>
2025-08-15 15:10:59 +0300

    fix CI

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -4,5 +4,5 @@ namespace Gendiff\Formatters\Json;
 
 function formatJson(array $diff): string
 {
-    return json_encode($diff, JSON_PRETTY_PRINT);
+    return json_encode($diff, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 }
